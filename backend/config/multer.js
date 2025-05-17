cb85fa1c113dff8a862f145cf55fcf486fb44975
Enzo Pinotti <enzopinottii@gmail.com>
2025-05-17 16:04:57 -0300

    add: aumento de limite de img de perfil a 10MB

--- backend/config/multer.js
+++ backend/config/multer.js
@@ -26,7 +26,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 5 * 1024 * 1024 }, // Límite de 5MB
+  limits: { fileSize: 10 * 1024 * 1024 }, //Limite de 10MB
 });
 
 export default upload;
